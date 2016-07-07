047acde1fa16ce87d5867ddda6afe3b5fb404f36
xyulex <raulmartinez911@hotmail.com>
2016-07-07 12:02:27 +0200

    Back button in gig detail component

--- dev/gig.detail.component.ts
+++ dev/gig.detail.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 import {ApiService} from './api.service';
-import {RouteParams} from 'angular2/router';
+import {RouteParams, Router} from 'angular2/router';
 
 @Component({
     template: `
@@ -11,16 +11,21 @@ import {RouteParams} from 'angular2/router';
             	<tr><td> {{ gigDetail.date | slice:0:10}} </td></tr>
                 <tr><td> {{ gigDetail.venue }} </td></tr>
                 <tr><td> {{ gigDetail.price | currency }} </td></tr>
+                <tr><td><button type="button" class="btn btn-info" (click)="goToIndex()"><< Back</button></td></tr>
         	</table>
         </div>`
 })
 
 export class GigDetailComponent {
-    constructor(private _apiservice: ApiService, params:RouteParams) {
+    constructor(private _apiservice: ApiService, params:RouteParams, private router: Router) {
         this._apiservice
             .getGigDetail(params.get('id'))
             .subscribe(gigDetail => this.gigDetail = gigDetail,
                 error => console.log('Gig detail error'),
                 () => console.log('Gig detail loaded' + this.gigDetail));    
+    }
+
+    goToIndex () {
+        this.router.navigate(['Gigs']);
     }
 }
